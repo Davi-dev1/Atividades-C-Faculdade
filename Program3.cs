60477cabe1b7c774bdd5a877b8ee2beabb5374ac
Davi_dev1 <davigancalveesdasilva@gmail.com>
2026-09-09 11:39:44 -0300

    Add files via upload

---
+++ Program3.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+double pi;
+int raio;
+double a;
+
+Console.WriteLine("Calcular a área do Circulo");
+
+Console.WriteLine("Digite o valor do raio");
+
+raio = int.Parse(Console.ReadLine());
+
+pi= 3.14;
+Console.WriteLine(pi);
+
+
+a = pi*raio*raio;
+
+
+Console.WriteLine("Àrea total do circulo "+ a.ToString("0.0000"));
+
+
+
+
+
---
+++ Program4.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int primeiroValor;
+int SegundoValor;
+int soma;
+
+Console.WriteLine("Digite o Valor de A ");
+primeiroValor = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite o Valor de B ");
+SegundoValor = int.Parse(Console.ReadLine());
+
+soma = primeiroValor + SegundoValor;
+Console.WriteLine("SOMA = " + soma);
